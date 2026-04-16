3a027ef4bb8fb22f7912c274c4e00ce4807e085d
Philippe Serhal <philippe.serhal@netlify.com>
2026-04-16 13:09:45 -0400

    feat: make a conflicting change in app/pages/index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -2,7 +2,7 @@
   <section>
     <h3>Rendering and caching strategies</h3>
 
-    <div class="panel">
+    <div class="panel a-fake-class">
       Keep in mind that when you click on a link below Nuxt performs client-side
       navigation, so no SSR nor prerendering is involved. Reload the page or
       open it in a new tab to see initial load behavior.
